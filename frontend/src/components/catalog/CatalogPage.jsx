5df58a1a7695555575aa304808ee3a4a9f61ac4e
Mauro Serrano Hevia <mserrano@stemdo.io>
2026-04-16 13:32:53 +0200

    Remove duplicate import of useCallback

--- frontend/src/components/catalog/CatalogPage.jsx
+++ frontend/src/components/catalog/CatalogPage.jsx
@@ -1,7 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
-import { useState, useEffect, useCallback } from "react";
 import "./CatalogPage.css";
 import {
   productosAPI,
